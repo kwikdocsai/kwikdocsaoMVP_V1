24d14fd8b15f5526d129ab83c0489c941b454bdf
conversioao <conversioao@gmail.com>
2026-02-01 20:13:26 +0000

    f

--- assets/chevron-up-CU1TZYCU.js
+++
@@ -1,6 +0,0 @@
-import{d as o}from"./index-js_cOUDW.js";/**
- * @license lucide-react v0.562.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const e=[["path",{d:"m18 15-6-6-6 6",key:"153udz"}]],n=o("chevron-up",e);export{n as C};
